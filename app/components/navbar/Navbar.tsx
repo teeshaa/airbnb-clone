89efec47f70e1d9017d98876456ffebbf992529f
Teesha Ghevariya <tishaghevariya@gmail.com>
2023-07-15 21:24:33 +0530

    Search Bar Added

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return (
@@ -21,6 +22,7 @@ const Navbar = () => {
                     md:gap-0"
                     >
                         <Logo />
+                        <Search/>
                     </div>
                 </Container>
 
